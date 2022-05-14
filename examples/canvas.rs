a257a9d12b28f7bafb5255811f19863a595fa206
Lachezar Lechev <elpiel93@gmail.com>
2022-05-14 22:35:39 +0200

    example - canvas - update example & add CanvasAt crop

--- examples/canvas.rs
+++ examples/canvas.rs
@@ -1,4 +1,4 @@
-use embedded_canvas::canvas::Canvas;
+use embedded_canvas::Canvas;
 use embedded_graphics::{
     pixelcolor::Rgb555,
     prelude::*,
@@ -16,9 +16,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // 1. Draw the Canvas with an outline rectangle (on the left)
     //
-    // 2. Draw on the center of the canvas and crop it (on the right):
+    // 2. Draw on the center of the canvas and crop it (on the top right):
     // - a rectangle - 100 x 100
     // - circle - 98 diameter (1 pixel on each side accounts for the rectangle size)
+    //
+    // 3. Take the second canvas and crop the rectangle. Draw it below 2. (on the bottom right)
     {
         let canvas_size = Size {
             width: 200,
@@ -64,22 +66,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let crop_area = Rectangle::with_center(canvas.center(), drawing_size);
 
         // crop the canvas
-        let cropped_canvas = canvas.crop(&crop_area).expect("Should crop");
+        let cropped_canvas = canvas.crop(&crop_area).expect("Should crop")
+        // place the cropped canvas 80px away from the original canvas with the outline
+        .place_at(Point {
+            x: canvas.bounding_box().size.width as i32 + 80,
+            y: 0,
+        });
 
         // draw the cropped Canvas on to the display and offset it by 80px to the right of the non-cropped one
+        cropped_canvas.draw(&mut display)?;
+
+        // Crop from the cropped canvas, only the circle
+        // remove the stroke of the drawn rectangle
+        let crop_rectangle =
+            Rectangle::with_center(cropped_canvas.center(), drawing_size - Size::new(2, 2));
         cropped_canvas
-            // place the cropped canvas 80px away from the original canvas with the outline
-            .place_at(Point {
-                x: canvas.bounding_box().size.width as i32 + 80,
-                y: 0,
-            })
+            .crop(&crop_rectangle)
+            .expect("Should crop")
+            .translate(Point::new(0, 100))
             .draw(&mut display)?;
     }
 
     let output_settings = OutputSettingsBuilder::new().build();
 
     Window::new(
-        "Canvas with outline (left) & cropped and drawn Canvas (right)",
+        "Canvas with outline (left) & 2 Canvas (right)",
         &output_settings,
     )
     .show_static(&display);
