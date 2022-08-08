14609ca5c2554e91e254fb6e185c14f32e6e32b0
Lachezar Lechev <elpiel93@gmail.com>
2022-08-08 23:15:09 +0200

    lib & README - update docs for CCanvas & CCanavasAt

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,10 @@
 //! Based on [`embedded-graphics-core`] and [`embedded-graphics`]
 //! (see `transform` feature in [Crate features](#crate-features)).
 //!
-//! This crate is `no_std` but requires `alloc` for the time being.
+//! This crate is `no_std` and it has 2 sets of canvases:
+//! - [`Canvas`] and [`CanvasAt`] - require `alloc` feature
+//! - [`CCanvas`] and [`CCanvasAt`] - do **not** require `alloc` feature because they
+//!  use const generics instead.
 //!
 //! The main advantages of the canvases in this crate are:
 //!
@@ -38,35 +41,39 @@
 //!
 //! There are **two** main canvases you can work with:
 //!
-//! ## `Canvas`
+//! ## `Canvas` / `CCanvas`
 //!
-//! A [`Canvas`] which you can draw on with origin [`Point::zero()`](embedded_graphics_core::geometry::Point::zero).
+//! A canvas which you can draw on with origin [`Point::zero()`](embedded_graphics_core::geometry::Point::zero).
 //! The canvas location is not set for the provided display.
 //!
 //! After drawing decide where to place it on the display using the methods:
-//! - [`Canvas::place_at(top_left: Point) -> CanvasAt`](Canvas::place_at)
-//! - [`Canvas::place_center(center: Point) -> CanvasAt`](Canvas::place_center)
+//! - `Canvas::place_at(top_left: Point) -> CanvasAt` (with `alloc` feature) or `CCanvas::place_at(top_left: Point) -> CCanvasAt`
+//! - `Canvas::place_center(center: Point) -> CanvasAt` (with `alloc` feature) or `CCanvas::place_center(center: Point) -> CCanvasAt`
 //!
-//! ## `CanvasAt`
+//! ## `CanvasAt` / `CCanavasAt`
 //!
-//! [`CanvasAt`] is a type of canvas ready to be drawn on the display
-//! at specified location (hence the name [`CanvasAt`]).
+//! [`CanvasAt`]/[`CCanvasAt`] is a type of canvas ready to be drawn on the display
+//! at specified location (hence the name [`CanvasAt`]/[`CCanvasAt`]).
 //!
-//! There are two ways of using [`CanvasAt`]:
+//! There are two ways of using [`CanvasAt`]/[`CCanvasAt`]:
 //!
-//! 1. Directly placing the [`CanvasAt`] on specified location on the display and drawing inside.
-//! 2. Create a [`Canvas`] and when ready to draw it on the display place the
-//!  [`Canvas`] at specified location using the methods:
-//!   - [`Canvas::place_at(top_left: Point) -> CanvasAt`](Canvas::place_at)
-//!   - [`Canvas::place_center(center: Point) -> CanvasAt`](Canvas::place_center)
+//! 1. Directly placing the [`CanvasAt`]/[`CCanvasAt`] on specified location on the display and drawing inside.
+//! 2. Create a [`Canvas`]/[`CCanvas`] and when ready to draw it on the display place the
+//!  [`Canvas`]/[`CCanvas`] at specified location using the methods:
+//!    - `Canvas::place_at(top_left: Point) -> CanvasAt` (with `alloc` feature) and `CCanvas::place_at(top_left: Point) -> CCanvasAt`
+//!    - `Canvas::place_center(center: Point) -> CanvasAt` (with `alloc` feature) and `CCanvas::place_center(center: Point) -> CCanvasAt`
 //!
 //! # Crate features
 //! - `default` features - `transform`
-//! - `transform` - enables the trait implementation of [`embedded_graphics::transform::Transform`] for [`CanvasAt`].
+//! - `alloc` - enables [`Canvas`] and [`CanvasAt`].
+//! - `transform` - enables the trait implementation of [`embedded_graphics::transform::Transform`] for [`CanvasAt`]/[`CCanvasAt`].
 //!
 //! [github]: https://img.shields.io/badge/github-3873AD?style=for-the-badge&labelColor=555555&logo=github
 //! [crates-io]: https://img.shields.io/crates/v/embedded-canvas?logo=rust&style=for-the-badge
 //! [sponsor-us]: https://img.shields.io/github/sponsors/LechevSpace?color=bf3989&label=Sponsor%20us&style=for-the-badge&logoColor=bf3989&logo=data%3Aimage%2Fsvg%2Bxml%3Bbase64%2CPHN2ZyBoZWlnaHQ9IjE2IiB2aWV3Qm94PSIwIDAgMTYgMTYiIHZlcnNpb249IjEuMSIgd2lkdGg9IjE2IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogICAgPHBhdGggZmlsbD0iI2JmMzk4OSIgZmlsbC1ydWxlPSJldmVub2RkIiBkPSJNNC4yNSAyLjVjLTEuMzM2IDAtMi43NSAxLjE2NC0yLjc1IDMgMCAyLjE1IDEuNTggNC4xNDQgMy4zNjUgNS42ODJBMjAuNTY1IDIwLjU2NSAwIDAwOCAxMy4zOTNhMjAuNTYxIDIwLjU2MSAwIDAwMy4xMzUtMi4yMTFDMTIuOTIgOS42NDQgMTQuNSA3LjY1IDE0LjUgNS41YzAtMS44MzYtMS40MTQtMy0yLjc1LTMtMS4zNzMgMC0yLjYwOS45ODYtMy4wMjkgMi40NTZhLjc1Ljc1IDAgMDEtMS40NDIgMEM2Ljg1OSAzLjQ4NiA1LjYyMyAyLjUgNC4yNSAyLjV6TTggMTQuMjVsLS4zNDUuNjY2LS4wMDItLjAwMS0uMDA2LS4wMDMtLjAxOC0uMDFhNy42NDMgNy42NDMgMCAwMS0uMzEtLjE3IDIyLjA3NSAyMi4wNzUgMCAwMS0zLjQzNC0yLjQxNEMyLjA0NSAxMC43MzEgMCA4LjM1IDAgNS41IDAgMi44MzYgMi4wODYgMSA0LjI1IDEgNS43OTcgMSA3LjE1MyAxLjgwMiA4IDMuMDIgOC44NDcgMS44MDIgMTAuMjAzIDEgMTEuNzUgMSAxMy45MTQgMSAxNiAyLjgzNiAxNiA1LjVjMCAyLjg1LTIuMDQ1IDUuMjMxLTMuODg1IDYuODE4YTIyLjA4IDIyLjA4IDAgMDEtMy43NDQgMi41ODRsLS4wMTguMDEtLjAwNi4wMDNoLS4wMDJMOCAxNC4yNXptMCAwbC4zNDUuNjY2YS43NTIuNzUyIDAgMDEtLjY5IDBMOCAxNC4yNXoiPjwvcGF0aD4KPC9zdmc%2BCg%3D%3D
+// #![deny(clippy::all)]
+#![deny(rustdoc::broken_intra_doc_links)]
+
 #![no_std]
 // only enables the `doc_cfg` feature when
 // the `docsrs` configuration attribute is defined
